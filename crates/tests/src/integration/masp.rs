ba86de7e94637ec16680f5c2fd7f14bcdccca14f
satan <jacob.turner870@gmail.com>
2025-07-25 16:11:00 +0200

    Formatting

--- crates/tests/src/integration/masp.rs
+++ crates/tests/src/integration/masp.rs
@@ -19,10 +19,8 @@ use namada_core::dec::Dec;
 use namada_core::masp::{MaspTxId, Precision, TokenMap, encode_asset_type};
 use namada_node::shell::ResultCode;
 use namada_node::shell::testing::client::{
-    dummy_args, run, sign_tx, submit_custom,
+    dummy_args, run, run, sign_tx, submit_custom,
 };
-
-use namada_node::shell::testing::client::run;
 use namada_node::shell::testing::node::NodeResults;
 use namada_node::shell::testing::utils::{Bin, CapturedOutput};
 use namada_sdk::account::AccountPublicKeysMap;

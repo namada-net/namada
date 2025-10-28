c6956ea699a3ef91aa5dd312b9d6cab7bb3c1ba5
yito88 <yito88@protonmail.com>
2025-10-28 14:17:30 +0100

    skip add_deposit for refund

--- crates/ibc/src/context/transfer_mod.rs
+++ crates/ibc/src/context/transfer_mod.rs
@@ -302,6 +302,7 @@ where
         acknowledgement: &Acknowledgement,
         relayer: &Signer,
     ) -> (ModuleExtras, Result<(), ChannelError>) {
+        self.ctx.enable_refund_transfer();
         let (extras, result) = on_acknowledgement_packet_execute(
             &mut self.ctx,
             packet,
@@ -325,6 +326,7 @@ where
         packet: &Packet,
         relayer: &Signer,
     ) -> (ModuleExtras, Result<(), ChannelError>) {
+        self.ctx.enable_refund_transfer();
         let (extras, result) =
             on_timeout_packet_execute(&mut self.ctx, packet, relayer);
         (extras, result.map_err(into_channel_error))
